f7fae06f3b80b1c65c35fb4615b6e6a6b07801bf
snielsen <snielsen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 21:48:41 +0000

    tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/ultralink/tags/1.0.0@947140 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- headers/globals.php
+++ headers/globals.php
@@ -7,7 +7,7 @@
 
     $APIversion = "https://ultralink.me/API/current/";
         
-    $ultralink_version = "32";
-    $ultralink_version_string = "0.9.2";
+    $ultralink_version = "36";
+    $ultralink_version_string = "1.0.0";
     
 ?>
