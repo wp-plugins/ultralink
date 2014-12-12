070c0a451e6a680863eb18754976289c2c7068ac
snielsen <snielsen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 21:31:33 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/ultralink/tags/1.0.1@1043700 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- headers/linkDetectors.js
+++ headers/linkDetectors.js
@@ -1,24 +1,48 @@
-var linkDetectors = Array()
-linkDetectors["(\.png|\.jpeg|\.jpg|\.gif|\.tiff|\.svg)$"]         = "image";
-linkDetectors["http.*gravatar\.com/avatar/"]                      = "image";
-linkDetectors["http.*wikipedia\.org/wiki/(?!(User|Wikipedia|File|MediaWiki|Template|Help|Category|Portal|Book|Education_Program|TimedText)(_talk)?:)"] = "wikipedia";
-linkDetectors["http.*freebase\.com"]                              = "freebase";
-linkDetectors["http.*angel\.co"]                                  = "angellist";
-linkDetectors["http.*ultralink\.me/annotation/"]                  = "annotation";
-linkDetectors["http.*www\.amazon\.com"]                           = "buyamazon";
-linkDetectors["http.*ebay\.com"]                                  = "buyebay";
-linkDetectors["http://click\.linksynergy\.com.*partnerId%253D30"] = "buylinkshareapple";
-linkDetectors["http://click\.linksynergy\.com"]                   = "buy";
-linkDetectors["http.*www\.imdb\.com"]                             = "imdb";
-linkDetectors["http.*developer\.apple\.com.*/Manpages/"]          = "manpage";
-linkDetectors["http.*opengl\.org.*/docs/man/"]                    = "manpage";
-linkDetectors["http.*developer\.palm\.com/appredirect"]           = "appwebos";
-linkDetectors["http.*linkedin\.com"]                              = "linkedin";
-linkDetectors["http.*facebook\.com"]                              = "facebook";
-linkDetectors["http.*twitter\.com"]                               = "twitter";
-linkDetectors["http.*plus\.google\.com"]                          = "googleplus";
-linkDetectors["http.*maps\.google\.com"]                          = "mapgoogle";
-linkDetectors["http.*youtube\.com"]                               = "videoyoutube";
-linkDetectors["http.*google\.com/search\?"]                       = "searchgoogle";
-linkDetectors["http.*search\.yahoo\.com/search\?"]                = "searchyahoo";
-linkDetectors["http.*bing\.com/search\?"]                         = "searchbing";
+var linkDetectors = {
+    "(\.png|\.jpeg|\.jpg|\.gif|\.tiff|\.svg|\.bmp)$":                'image',
+    "http.*gravatar\\.com/avatar/":                                  'image',
+    "(\.pdf)$":                                                      'pdf',
+    "http.*wikipedia\\.org/wiki/(?!(User|Wikipedia|File|MediaWiki|Template|Help|Category|Portal|Book|Education_Program|TimedText)(_talk)?:)": 'wikipedia',
+    "http.*freebase\\.com":                                          'freebase',
+    "http.*angel\\.co":                                              'angellist',
+    "http.*crunchbase\\.com/(company|person|organization)":          'crunchbase',
+    "http.*ultralink\\.me/annotation/":                              'annotation',
+    "http.*www\\.amazon\\.(br|ca|com|co\.uk|cn|de|es|fr|in|it|jp)/": 'buyamazon',
+    "http.*ebay\.com":                                               'buyebay',
+    "http://click\\.linksynergy\\.com.*partnerId%253D30":            'buylinkshareapple',
+    "http://click\\.linksynergy\\.com":                              'buy',
+    "http.*itunes\\.apple\\.com":                                    'buyapple',
+    "http.*www\\.imdb\\.com":                                        'imdb',
+    "http.*developer\\.apple\\.com.*/Manpages/":                     'manpage',
+    "http.*opengl\\.org.*/docs/man/":                                'manpage',
+    "http.*developer\\.palm\\.com/appredirect":                      'appwebos',
+    "http.*play\\.google\\.com/store/apps/details":                  'appandroid',
+    "http.*linkedin\\.com":                                          'linkedin',
+    "http.*facebook\\.com":                                          'facebook',
+    "http.*twitter\\.com":                                           'twitter',
+    "http.*plus\\.google\\.com":                                     'googleplus',
+    "http.*maps\\.google\\.com":                                     'mapgoogle',
+    "http.*youtube\\.com":                                           'videoyoutube',
+    "http.*vimeo\\.com":                                             'videovimeo',
+    "http.*github\\.com":                                            'github',
+    "http.*google\\.com/search\?":                                   'searchgoogle',
+    "http.*search\\.yahoo\\.com/search\?":                           'searchyahoo',
+    "http.*bing\\.com/search\?":                                     'searchbing',
+    "http.*ark\\.intel\\.com/products\/":                            'intelark',
+    "http.*comicvine\\.com\/":                                       'comicvine',
+    "http.*webmd\\.com\/":                                           'webmd',
+    "xmpp:.*":                                                       'xmpp'
+};
+
+function linkType( url )
+{
+    for( var detector in linkDetectors )
+    {
+        if( url.match(RegExp(detector, "i")) )
+        {
+            return linkDetectors[detector];
+        }
+    }
+    
+    return 'href';
+}
--- headers/linkTypes.js
+++ headers/linkTypes.js
@@ -1,34 +1,81 @@
-var linkTypes = Array()
-linkTypes["image"]             = "Image";
-linkTypes["href"]              = "Link";
-linkTypes["href2"]             = "Link 2";
-linkTypes["href3"]             = "Link 3";
-linkTypes["buy"]               = "Buy";
-linkTypes["video"]             = "Video";
-linkTypes["videoyoutube"]      = "YouTube";
-linkTypes["buylinkshareapple"] = "Buy From Apple";
-linkTypes["buyamazon"]         = "Buy From Amazon";
-linkTypes["buyebay"]           = "Buy From Ebay";
-linkTypes["appios"]            = "iOS App";
-linkTypes["appmac"]            = "Mac App";
-linkTypes["appwebos"]          = "webOS App";
-linkTypes["appandroid"]        = "Android App";
-linkTypes["appwindows"]        = "Windows App";
-linkTypes["github"]            = "GitHub";
-linkTypes["imdb"]              = "IMDB Profile";
-linkTypes["manpage"]           = "Documentation";
-linkTypes["twitter"]           = "Twitter";
-linkTypes["linkedin"]          = "LinkedIn";
-linkTypes["facebook"]          = "Facebook";
-linkTypes["googleplus"]        = "Google+";
-linkTypes["wikipedia"]         = "Wikipedia";
-linkTypes["mediawiki"]         = "MediaWiki";
-linkTypes["freebase"]          = "Freebase";
-linkTypes["angellist"]         = "AngelList";
-linkTypes["annotation"]        = "Annotation";
-linkTypes["map"]               = "Map";
-linkTypes["mapgoogle"]         = "Google Map";
-linkTypes["search"]            = "Search";
-linkTypes["searchgoogle"]      = "Google Search";
-linkTypes["searchyahoo"]       = "Yahoo Search";
-linkTypes["searchbing"]        = "Bing Search";
+var linkTypes = {
+    'image':             "Image",
+    'pdf':               "PDF",
+    'href':              "Link",
+    'href2':             "Link 2",
+    'href3':             "Link 3",
+    'buy':               "Buy",
+    'video':             "Video",
+    'videoyoutube':      "YouTube",
+    'videovimeo':        "Vimeo",
+    'videovlc':          "VLC",
+    'buylinkshareapple': "Buy From Apple",
+    'buyapple':          "Buy From Apple",
+    'buyamazon':         "Buy From Amazon",
+    'buyebay':           "Buy From Ebay",
+    'appios':            "iOS App",
+    'appmac':            "Mac App",
+    'appwebos':          "webOS App",
+    'appandroid':        "Android App",
+    'appwindows':        "Windows App",
+    'github':            "GitHub",
+    'imdb':              "IMDB Profile",
+    'espn':              "ESPN Profile",
+    'webmd':             "WebMD",
+    'manpage':           "Documentation",
+    'twitter':           "Twitter",
+    'linkedin':          "LinkedIn",
+    'facebook':          "Facebook",
+    'googleplus':        "Google+",
+    'wikipedia':         "Wikipedia",
+    'mediawiki':         "MediaWiki",
+    'freebase':          "Freebase",
+    'angellist':         "AngelList",
+    'crunchbase':        "CrunchBase",
+    'intelark':          "Intel ARK",
+    'comicvine':         "Comic Vine",
+    'annotation':        "Annotation",
+    'map':               "Map",
+    'mapgoogle':         "Google Map",
+    'xmpp':              "Jabber",
+    'search':            "Search",
+    'searchgoogle':      "Google Search",
+    'searchyahoo':       "Yahoo Search",
+    'searchbing':        "Bing Search",
+    'searchpubmed':      "PubMed Search",
+    'searchul':          "Search Ultralinks"
+};
+
+var linkTypeCategories = {
+    'Image':      [ "image" ],
+    'Link':       [ "href", "href2", "href3" ],
+    'App':        [ "appios", "appmac", "appwebos", "appandroid", "appwindows" ],
+    'Buy':        [ "buy", "buylinkshareapple", "buyapple", "buyamazon", "buyebay" ],
+    'Social':     [ "twitter", "linkedin", "googleplus", "facebook" ],
+    'Reference':  [ "wikipedia", "mediawiki", "imdb", "espn", "webmd", "github", "freebase", "angellist", "crunchbase", "manpage", "map", "mapgoogle", "intelark", "comicvine" ],
+    'Video':      [ "video", "videoyoutube", "videovimeo", "videovlc" ],
+    'Annotation': [ "annotation" ],
+    'Chat':       [ "xmpp" ],
+    'Search':     [ "searchul", "search", "searchgoogle", "searchyahoo", "searchbing", "searchpubmed" ]
+};
+
+var linkInlineDependencies = {
+    'wikipedia':    "",
+    'mediawiki':    "",
+    'annotation':   "",
+    'videoyoutube': "",
+    'videovimeo':   "",
+    'mapgoogle':    "",
+    'ultralinkme':  "",
+    'twitter':      "twitter",
+    'linkedin':     "linkedin",
+    'googleplus':   "googleplus",
+    'facebook':     "facebook",
+    'angellist':    "angellist",
+    'crunchbase':   "ultralinkme",
+    'buyamazon':    "ultralinkme",
+    'intelark':     "ultralinkme",
+    'comicvine':    "ultralinkme"
+};
+
+var customLinkTypes = {};
